b964e182390bbd4f2d0bccf6851b59934f41c784
Antony Southworth <antony.southworth@quantiful.co.nz>
2019-05-06 10:02:16 +1200

    add rust

---
+++ rust/s256/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use std::io::Read;
+use std::process::exit;
+
+use sha2::Sha256;
+use sha2::digest::Digest;
+
+fn main()
+{
+    let mut hasher = Sha256::default();
+    for byte in io::stdin().lock().bytes()
+    {
+        match byte
+        {
+            Ok(b) => hasher.input(&[b]),
+            Err(e) =>
+            {
+                eprintln!("[s256] failed to read byte! {}", e);
+                exit(1);
+            }
+        }
+    }
+
+    println!("{}", hex::encode(hasher.result()));
+
+    exit(0);
+}
